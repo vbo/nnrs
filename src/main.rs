876b3591e955d54c60af4c74ca1acd376ea35895
Vadim Borodin <borodin.vadim@gmail.com>
2018-04-08 00:45:21 -0700

    loading training data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 extern crate rand;
+extern crate byteorder;
 
 use std::fmt;
+use std::fs::File;
+use std::io::Read;
+
 use rand::distributions::{IndependentSample, Range};
+use byteorder::{ByteOrder, BigEndian};
 
 #[derive(Debug)]
 struct Matrix {
@@ -10,23 +15,86 @@ struct Matrix {
     rows: usize,
 }
 
-static WRITE_ERR: &'static str = "Failed to write";
+struct TrainingData {
+    input_mem: Vec<f64>,
+    label_mem: Vec<f64>,
+    example_indices: Vec<usize>,
+    examples_count: usize,
+    input_size: usize,
+    label_size: usize,
+}
 
-impl fmt::Display for Matrix {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Matrix {}x{}:\n", self.rows, self.cols).expect(&WRITE_ERR);
-        for row in 0..self.rows {
-            let row_start = row*self.cols;
-            for col in 0..self.cols {
-                write!(f, "{:8.4}", self.mem[row_start + col]).expect(&WRITE_ERR);
+fn load_mnist_training_data() -> TrainingData {
+    const MNIST_IMAGES_FILE: &str = "data/train-images-idx3-ubyte.bin";
+    const MNIST_LABELS_FILE: &str = "data/train-labels-idx1-ubyte.bin";
+    let mut images_file = File::open(MNIST_IMAGES_FILE).unwrap();
 
-            }
-            write!(f, "\n").expect(&WRITE_ERR);
+    let mut header_buf = [0u8; 16];
+    images_file.read_exact(&mut header_buf).unwrap();
+    let images_count = BigEndian::read_i32(&header_buf[4..]) as usize;
+    let rows = BigEndian::read_i32(&header_buf[8..]) as usize;
+    let cols = BigEndian::read_i32(&header_buf[12..]) as usize;
 
+    let input_size = rows*cols;
+    let images_data_size = images_count*input_size;
+    let mut images_data = Vec::<f64>::with_capacity(images_data_size);
+    let mut read_buf = [0u8; 1024*1024];
+    loop {
+        let bytes_read = images_file.read(&mut read_buf).unwrap();
+        if bytes_read == 0 {
+            break;
         }
 
-        return Ok(());
+        for i in 0..bytes_read {
+            images_data.push((read_buf[i] as f64)/255.0);
+        }
     }
+
+    let mut labels_file = File::open(MNIST_LABELS_FILE).unwrap();
+
+    let mut header_buf = [0u8; 8];
+    labels_file.read_exact(&mut header_buf).unwrap();
+    let labels_count = BigEndian::read_i32(&header_buf[4..]) as usize;
+
+    assert!(labels_count == images_count,
+            "Invalid training data. Labels count != inputs count");
+
+    let label_size = 10usize;
+    let labels_data_size = labels_count*label_size;
+    let mut labels_data = Vec::<f64>::with_capacity(labels_data_size);
+
+    loop {
+        let bytes_read = labels_file.read(&mut read_buf).unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+
+        for i in 0..bytes_read {
+            let mut label_found = false;
+            for j in 0..10 {
+                let mut data = 0.0f64;
+                if read_buf[i] as usize == j {
+                    data = 1.0f64;
+                    label_found = true;
+                }
+
+                labels_data.push(data);
+            }
+
+            assert!(label_found, "Label not found byte: {}.", read_buf[i]);
+        }
+    }
+
+    let res = TrainingData {
+        input_mem: images_data,
+        label_mem: labels_data,
+        example_indices: (0usize..images_count).collect(),
+        examples_count: images_count,
+        input_size: input_size,
+        label_size: label_size,
+    };
+
+    return res;
 }
 
 impl Matrix {
@@ -123,6 +191,25 @@ impl Matrix {
     }
 }
 
+const WRITE_ERR: &str = "Failed to write";
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Matrix {}x{}:\n", self.rows, self.cols).expect(&WRITE_ERR);
+        for row in 0..self.rows {
+            let row_start = row*self.cols;
+            for col in 0..self.cols {
+                write!(f, "{:8.4}", self.mem[row_start + col]).expect(&WRITE_ERR);
+
+            }
+            write!(f, "\n").expect(&WRITE_ERR);
+
+        }
+
+        return Ok(());
+    }
+}
+
 
 #[derive(Debug)]
 struct Vector {
@@ -181,6 +268,15 @@ impl Vector {
         self.mem = v.mem.to_vec();
     }
 
+    pub fn copy_from_slice(&mut self, s: &[f64]) {
+        assert!(self.rows == s.len(),
+                "Invalid dimentions for copy_from_slice: {}x1 vs {}x1",
+                self.rows, s.len()); 
+        for i in 0..self.rows {
+            self.mem[i] = s[i];
+        }
+    }
+
     pub fn sub(&self, v: &Vector, res: &mut Vector) {
         assert!(self.rows == v.rows && self.rows == res.rows,
                 "Invalid dimentions for sub: {}x1 - {}x1 = {}x1",
@@ -217,6 +313,21 @@ impl Vector {
         }
         return res.sqrt();
     }
+
+    pub fn max_component(&self) -> (usize, f64) {
+        let mut max = self.mem[0];
+        let mut max_i = 0;
+
+        for i in 1..self.rows {
+            let val = self.mem[i];
+            if val > max {
+                max = val;
+                max_i = i;
+            }
+        }
+
+        return (max_i, max);
+    }
 }
 
 fn calc_weights_pd(
@@ -249,22 +360,48 @@ fn calc_bias_pd(
     }
 }
 
+fn render_training_example(label_data: &[f64], input_data: &[f64]) {
+    for i in 0..10 {
+        if label_data[i] > 0.0 {
+            println!("Label: {}", i);
+        }
+    }
+
+    for i in 0..28 {
+        let row_start = i*28;
+        for j in 0..28 {
+            let v = input_data[row_start + j];
+            if v > 0.7 {
+                print!("1");
+            } else {
+                print!("0");
+            }
+        }
+        print!("\n");
+    }
+}
+
 // Sizes
-const N_INPUTS: usize = 2;
-const N_L1: usize = 8;
-const N_L2: usize = 4;
-const N_OUTPUTS: usize = 2;
+const N_INPUTS: usize = 28*28;
+const N_L1: usize = 16;
+const N_L2: usize = 16;
+const N_OUTPUTS: usize = 10;
 
 const BATCH_SIZE: usize = 1000;
 const LOG_EVERY_N: usize = 100_000;
 const LEARNING_RATE: f64 = 1.0;
-const NUM_EPOCHS: usize = BATCH_SIZE*10_000;
+const NUM_EPOCHS: usize = 1000 * 60000;
 
 fn sigmoid(x: f64) -> f64 { 1.0 / ((-x).exp() + 1.0) }
 
 // Weights matrix:
 // Each row represents weights of edges between a given target node and all source nodes.
 fn main() {
+    let training_data = load_mnist_training_data();
+
+    assert!(training_data.input_size == N_INPUTS, "Wrong inputs!");
+    assert!(training_data.label_size == N_OUTPUTS, "Wrong outputs!");
+
     let mut inputs = Vector::new(N_INPUTS).init_with(1.0);
 
     let mut l1_weights = Matrix::new(N_L1, N_INPUTS).init_rand();
@@ -307,9 +444,26 @@ fn main() {
     let avg_by_batch = |x| { x * LEARNING_RATE / BATCH_SIZE as f64 };
     let set_0 = |_: f64| 0.0;
 
+
+    let mut current_examples_cursor = 0usize;
+    let mut hits = 0usize;
     for i in 0..NUM_EPOCHS {
         // Training data
-        inputs.fill_rand();
+        let current_example_index = training_data.example_indices[current_examples_cursor];
+        let input_data_offset = current_example_index*training_data.input_size;
+        let input_data_end = input_data_offset + training_data.input_size;
+        let input_data = &training_data.input_mem[input_data_offset..input_data_end];
+
+        let label_data_offset = current_example_index*training_data.label_size;
+        let label_data_end = label_data_offset + training_data.label_size;
+        let label_data = &training_data.label_mem[label_data_offset..label_data_end];
+        inputs.copy_from_slice(input_data);
+        true_outputs.copy_from_slice(label_data);
+
+        current_examples_cursor += 1;
+        if current_examples_cursor >= training_data.examples_count {
+            current_examples_cursor = 0;
+        }
 
         // Forward propagation
         l1_weights.dot_vec(&inputs, &mut l1_activations);
@@ -325,9 +479,13 @@ fn main() {
         outputs.apply(sigmoid);
 
         // Error
-        true_outputs.copy_from(&inputs);
         true_outputs.sub(&outputs, &mut error);
         avg_error += error.get_length();
+        let (max_i, max) = outputs.max_component();
+        let (tmax_i, tmax) = true_outputs.max_component();
+        if max_i == tmax_i {
+            hits += 1;
+        }
 
         // Backward propagation
         // Output layer
@@ -354,15 +512,17 @@ fn main() {
 
         if i % LOG_EVERY_N == 0 {
             println!("error over last {}: {:8.4}", LOG_EVERY_N, avg_error / LOG_EVERY_N as f64);
+            println!("hits {}%", (hits as f64) * 100.0 / (LOG_EVERY_N as f64));
             avg_error = 0.0;
+            hits = 0;
             if i % (BATCH_SIZE * 100) == 0 {
-                println!("l1_weights:{}", l1_weights);
-                println!("l1_bias:{}", l1_bias);
-                println!("l2_weights:{}", l2_weights);
-                println!("l2_bias:{}", l2_bias);
-                println!("output_weights:{}", output_weights);
-                println!("output_bias:{}", output_bias);
-                println!("output:{}", outputs);
+                //println!("l1_weights:{}", l1_weights);
+                //println!("l1_bias:{}", l1_bias);
+                //println!("l2_weights:{}", l2_weights);
+                //println!("l2_bias:{}", l2_bias);
+                //println!("output_weights:{}", output_weights);
+                //println!("output_bias:{}", output_bias);
+                //println!("output:{}", outputs);
             }
         }
         if i % BATCH_SIZE == 0 {
@@ -397,11 +557,11 @@ fn main() {
     }
 
 
-    println!("after l1_weights:{}", l1_weights);
-    println!("after l1_bias:{}", l1_bias);
-    println!("after l2_weights:{}", l2_weights);
-    println!("after l2_bias:{}", l2_bias);
-    println!("after output_weights:{}", output_weights);
-    println!("after output_bias:{}", output_bias);
-    println!("after error:{}", error);
+    //println!("after l1_weights:{}", l1_weights);
+    //println!("after l1_bias:{}", l1_bias);
+    //println!("after l2_weights:{}", l2_weights);
+    //println!("after l2_bias:{}", l2_bias);
+    //println!("after output_weights:{}", output_weights);
+    //println!("after output_bias:{}", output_bias);
+    //println!("after error:{}", error);
 }
